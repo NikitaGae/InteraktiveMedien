a13b0fd45acbb95da0378ea1b85cb22053c4c331
Nikita <nikita.gaertner@hs-furtwangen.de>
2023-12-11 13:53:58 +0100

    commit message

---
+++ island.js
@@ -0,0 +1,86 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { ARButton } from './ARButton.js';
+
+// camera configuration
+const FOV = 75;
+const near_plane = 0.1;
+const far_plane = 1000;
+
+// scene
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(FOV, window.innerWidth / window.innerHeight, near_plane, far_plane);
+const collection = new THREE.Object3D();
+scene.add(collection);
+collection.position.z = -3;
+collection.scale.divideScalar(3);
+
+// light
+/// light from the sky
+const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6);
+hemisphereLight.color.setHSL(0.6, 1, 1);
+hemisphereLight.groundColor.setHSL(0.095, 1, 0.75);
+hemisphereLight.position.set(0, 10, 0);
+scene.add(hemisphereLight)
+const hemiLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 1);
+hemiLightHelper.visible = false;
+scene.add(hemiLightHelper);
+
+/// light from the sun
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.color.setHSL(0.9, 1, 0.9);
+directionalLight.position.set(-2.5, 10, -2.5);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.width = directionalLight.shadow.mapSize.height = 2048
+scene.add(directionalLight);
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 1);
+directionalLightHelper.visible = false;
+scene.add(directionalLightHelper);
+
+// renderer
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setClearColor(0x000000, 0);
+renderer.setSize(window.innerWidth, window.innerHeight);
+var dom = renderer.domElement;
+renderer.xr.enabled = true;
+document.body.appendChild(ARButton.createButton(renderer));
+document.body.appendChild(dom);
+
+// object loadingzhutfztfgv
+const raum = new THREE.Object3D();
+const loader = new GLTFLoader();
+
+// orbit controls
+let controls;
+controls = new OrbitControls(camera, dom);
+controls.target.set(0, 1.6, 0);
+controls.update();
+
+// loading in the 3D models, saving them into usable variables and adding them to the scene.
+loader.load('models/raum.glb', function (gltf) {
+    // its always children[0] because the child gets removed from gltf.scene once you add it to the actual scene
+    console.log('Model loaded:', gltf);
+    raum.add(gltf.scene);
+    raum.name = "raum";
+    collection.add(raum);
+    raum.scale.set(10, 10, 10);
+
+}, undefined, function (error) {
+    console.error(error);
+});
+
+function animate() {
+    // scaling after everything has loaded
+    collection.scale.set(0.2, 0.2, 0.2);
+    collection.position.set(0, 0, 0);
+
+    renderer.setAnimationLoop(function () {
+        // rendering
+        renderer.outputEncoding = THREE.sRGBEncoding;
+        renderer.shadowMap.enabled = true;
+        renderer.render(scene, camera);
+    })
+}
+
+animate();
