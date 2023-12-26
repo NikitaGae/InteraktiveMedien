ba53750998379de99ddfc8bf2b1e0b3daaf22fc0
Nikita <nikita.gaertner@hs-furtwangen.de>
2023-12-26 17:34:43 +0100

    test

--- island.js
+++ island.js
@@ -1,3 +1,111 @@
+/* import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { ARButton } from './ARButton.js';
+
+// camera configuration
+const FOV = 75;
+const near_plane = 0.1;
+const far_plane = 1000;
+
+// scene
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(FOV, window.innerWidth / window.innerHeight, near_plane, far_plane);
+const collection = new THREE.Object3D();
+scene.add(collection);
+collection.position.z = -3;
+collection.scale.divideScalar(3);
+
+// light
+/// light from the sky
+const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6);
+hemisphereLight.color.setHSL(0.6, 1, 1);
+hemisphereLight.groundColor.setHSL(0.095, 1, 0.75);
+hemisphereLight.position.set(0, 10, 0);
+scene.add(hemisphereLight)
+const hemiLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 1);
+hemiLightHelper.visible = false;
+scene.add(hemiLightHelper);
+//ffffffff
+
+/// light from the sun
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.color.setHSL(0.9, 1, 0.9);
+directionalLight.position.set(-2.5, 10, -2.5);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.width = directionalLight.shadow.mapSize.height = 2048
+scene.add(directionalLight);
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 1);
+directionalLightHelper.visible = false;
+scene.add(directionalLightHelper);
+
+// renderer
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setClearColor(0x000000, 0);
+renderer.setSize(window.innerWidth, window.innerHeight);
+var dom = renderer.domElement;
+renderer.xr.enabled = true;
+document.body.appendChild(ARButton.createButton(renderer));
+document.body.appendChild(dom);
+
+// object loading
+const raum = new THREE.Object3D();
+const loader = new GLTFLoader();
+
+// orbit controls
+let controls;
+controls = new OrbitControls(camera, dom);
+controls.target.set(0, 1.6, 0);
+controls.update();
+
+loader.load('models/raum.glb', function (gltf) {
+    // its always children[0] because the child gets removed from gltf.scene once you add it to the actual scene
+
+    gltf.scene.traverse((child) => {
+        if (child.isMesh) {
+            // Load the texture using TextureLoader
+            const textureLoader = new THREE.TextureLoader();
+            const texture = textureLoader.load('models/DefaultMaterial_basecolor.png');
+
+            // Apply the texture to the mesh's material
+            child.material = new THREE.MeshStandardMaterial({
+                map: texture,
+                metalness: 0.0, // Adjust these properties based on your model
+                roughness: 0.0,
+            });
+        }
+    });
+
+
+
+    console.log('Model loaded:', gltf);
+    raum.add(gltf.scene);
+    raum.name = "raum";
+    collection.add(raum);
+    raum.scale.set(10, 10, 10);
+    
+}, undefined, function (error) {
+    console.error(error);
+});
+
+function animate() {
+    // scaling after everything has loaded
+    collection.scale.set(2, 2, 2);
+    collection.position.set(0, 0, 0);
+
+    renderer.setAnimationLoop(function () {
+        // time management
+        /// scaling to seconds
+        // rendering
+        renderer.outputEncoding = THREE.sRGBEncoding;
+        renderer.shadowMap.enabled = true;
+        renderer.render(scene, camera);
+    })
+}
+
+animate(); */
+
+
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
@@ -60,15 +168,41 @@ controls.update();
 
 loader.load('models/raum.glb', function (gltf) {
     // its always children[0] because the child gets removed from gltf.scene once you add it to the actual scene
+
+    gltf.scene.traverse((child) => {
+        if (child.isMesh) {
+            // Load the texture using TextureLoader
+            const textureLoader = new THREE.TextureLoader();
+            const texture = textureLoader.load('models/DefaultMaterial_basecolor.png');
+
+            // Apply the texture to the mesh's material
+            child.material = new THREE.MeshStandardMaterial({
+                map: texture,
+                metalness: 0.0, // Adjust these properties based on your model
+                roughness: 0.0,
+            });
+        }
+    });
+
+
+
     console.log('Model loaded:', gltf);
     raum.add(gltf.scene);
     raum.name = "raum";
     collection.add(raum);
     raum.scale.set(10, 10, 10);
-
+    
 }, undefined, function (error) {
     console.error(error);
 });
+
+function playSound(audioName) {
+    let audio = new Audio(audioName);
+    audio.loop = true;
+    audio.play();
+}
+
+playSound('model/Sad_End.wav');
 
 function animate() {
     // scaling after everything has loaded
